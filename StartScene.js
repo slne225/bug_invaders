d4626e2e5c920ba4b85ea4efbfe840d746b07fc7
slne225 <94694817+slne225@users.noreply.github.com>
2023-06-10 09:40:39 -0400

    StartScene error

--- StartScene.js
+++ StartScene.js
@@ -5,7 +5,7 @@ class StartScene extends Phaser.Scene {
 
 
     create() {
-        this.addEventListener.text( 150, 250, 'click to start', {fill: '#000000', fontSize: '20px'})
+        this.add.text( 150, 250, 'click to start', {fill: '#000000', fontSize: '20px'})
         this.input.on('pointerdown', () => {
             this.scene.stop('StartScene')
             this.scene.start('GameScene')
